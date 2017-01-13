62c2c553bc2ab66deb5c212e99b0d973c11afaf5
Matthew Parker <mparker2@sheffield.ac.uk>
2017-01-13 10:27:23 +0000

    parallel process bands

--- zonal_stats.sh
+++ zonal_stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#$ -l rmem=15G
-#$ -l mem=15G
+#$ -l rmem=1G
+#$ -l mem=1G
 #$ -j y
 #$ -N zonal_stats
 set -eo pipefail
@@ -16,10 +16,14 @@ echo "contains $BANDS bands"
 
 for B in `seq 1 "${BANDS}"`; do
   TSV="${RASTER%.*}_${B}_zonal_stats.tsv"
-  echo "processing band $B"
-  fio cat $SHAPEFILE | rio zonalstats \
-    -r $RASTER --band $B \
-    --nodata=0 \
-    --stats "min max mean std" | \
-  jq -r '.features[] | [.id, .properties._max, .properties._min, .properties._mean, .properties._std] | @tsv' > $TSV
+  qsub -cwd -j y -N "zonalstats_${B}" <<DELIM
+#!/bin/bash
+#$ -l mem=5G
+#$ -l rmem=5G
+fio cat $SHAPEFILE | rio zonalstats \
+  -r $RASTER --band $B \
+  --nodata=0 \
+  --stats "min max mean std" | \
+jq -r '.features[] | [.id, .properties._max, .properties._min, .properties._mean, .properties._std] | @tsv' > $TSV
+DELIM
 done
